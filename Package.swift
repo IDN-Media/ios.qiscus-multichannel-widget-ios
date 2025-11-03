df4f80fe60df09965138e693d6887025c26e2cfa
Timotius Leonardo <lionato14@gmail.com>
2025-11-03 22:14:56 +0700

    Chore: Add Image WebPCoder

--- Package.swift
+++ Package.swift
@@ -17,7 +17,6 @@ let package = Package(
         .package(url: "https://github.com/Alamofire/AlamofireImage.git", .upToNextMajor(from: "4.3.0")),
         .package(url: "https://github.com/IDN-Media/ios.SDWebImageWebPCoder.git", from: "1.0.0"),
         .package(url: "https://github.com/qiscus/QiscusCore-iOS.git", from: "1.14.2"),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", .upToNextMajor(from: "5.21.3")),
     ],
     targets: [
         .target(
@@ -25,8 +24,8 @@ let package = Package(
             dependencies: [
                 "SwiftyJSON",
                 "AlamofireImage",
-                .product(name: "QiscusCore", package: "QiscusCore-iOS"),
-                "SDWebImage"
+                "SDWebImageWebPCoder",
+                .product(name: "QiscusCore", package: "QiscusCore-iOS")
             ],
             path: "Sources/QiscusMultichannelWidget",
 	    resources: [
