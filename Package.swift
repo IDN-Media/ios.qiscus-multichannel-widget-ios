ea77ba90e3381feef76a02909850a2f223118984
Timotius Leonardo <lionato14@gmail.com>
2025-11-03 21:21:04 +0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,36 +1,30 @@
 // swift-tools-version:5.6
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "QiscusMultichannelWidget",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v11) // iOS 10 support is extremely old and not always supported by dependencies
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "QiscusMultichannelWidget",
-            targets: ["QiscusMultichannelWidget"]),
+            targets: ["QiscusMultichannelWidget"]
+        ),
     ],
-	
-	dependencies: [
-        // Dependencies declare other packages that this package depends on.
+    dependencies: [
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.2"),
         .package(url: "https://github.com/Alamofire/AlamofireImage.git", .upToNextMajor(from: "4.3.0")),
-	.package(url: "https://github.com/IDN-Media/ios.SDWebImageWebPCoder.git", from: "1.0.0"),
-	.package(url: "https://github.com/qiscus/QiscusCore-iOS.git", from: "1.14.2")
+        .package(url: "https://github.com/IDN-Media/ios.SDWebImageWebPCoder.git", from: "1.0.0"),
+        .package(url: "https://github.com/qiscus/QiscusCore-iOS.git", from: "1.14.2")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "QiscusMultichannelWidget",
             dependencies: [
                 "SwiftyJSON",
                 "AlamofireImage",
-		        "SDWebImageWebPCoder",
+                "SDWebImageWebPCoder",
                 .product(name: "QiscusCore", package: "QiscusCore-iOS")
             ],
             path: "Sources/QiscusMultichannelWidget",
