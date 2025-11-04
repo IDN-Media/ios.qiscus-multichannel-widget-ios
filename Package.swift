0fdfdd824115633b8718abac40c6cafae9e6b9f2
Timotius Leonardo <lionato14@gmail.com>
2025-11-04 15:56:30 +0700

    Chore: checkout to 2.2.2 origin

--- Package.swift
+++ Package.swift
@@ -1,44 +1,33 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.6
 import PackageDescription
 
 let package = Package(
-    name: "MultichannelWidget",
+    name: "QiscusMultichannelWidget",
     platforms: [
-        .iOS(.v10)
+        .iOS(.v11) // iOS 10 support is extremely old and not always supported by dependencies
     ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "MultichannelWidget",
-            targets: ["MultichannelWidget"]),
+            name: "QiscusMultichannelWidget",
+            targets: ["QiscusMultichannelWidget"]
+        ),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON", from: "5.0.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "4.9.0")),
-        .package(url: "https://github.com/Alamofire/AlamofireImage.git", .upToNextMajor(from: "3.6.0")),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),
-        .package(url: "https://github.com/Qiscus-Integration/QiscusCoreApi.git", .branch ("spm"))
+        .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "5.0.2"),
+        .package(url: "https://github.com/Alamofire/AlamofireImage.git", .upToNextMajor(from: "4.3.0")),
+        .package(url: "https://github.com/IDN-Media/ios.SDWebImageWebPCoder.git", from: "1.0.1"),
+        .package(url: "https://github.com/IDN-Media/ios.QiscusCore-iOS.git", from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "MultichannelWidget",
-            dependencies: ["SwiftyJSON", "Alamofire", "AlamofireImage", "SDWebImage", "QiscusCoreAPI"],
-            resources: [
-                .bundle(name: "MultichannelWidget", content: [
-                    "Sources/MultichannelWidget/**/*.{h,m,swift,xib}": .resource
-                ]),
-                .include(content: [
-                    "Sources/MultichannelWidget/**/*.png": .xcassets
-                ])
-            ]
+            name: "QiscusMultichannelWidget",
+            dependencies: [
+                "SwiftyJSON",
+                "AlamofireImage",
+                .product(name: "SDWebImageWebPCoder", package: "ios.SDWebImageWebPCoder"),
+                .product(name: "QiscusCore", package: "ios.QiscusCore-iOS")
+            ],
+            path: "Sources/QiscusMultichannelWidget",
         ),
-        .testTarget(
-            name: "MultichannelWidgetTests",
-            dependencies: ["MultichannelWidget"]),
     ]
 )
